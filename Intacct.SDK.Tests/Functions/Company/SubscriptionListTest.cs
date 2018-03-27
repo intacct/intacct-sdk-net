9379f43cce91318b6647f9bd8bff0f6482613b5a
fpmess <37664720+fpmess@users.noreply.github.com>
2018-03-27 16:21:59 -0500

    Resolve issue #30
    Adding functions to
     - list subscriptions, and
    - get user effective permissions

---
+++ Intacct.SDK.Tests/Functions/Company/SubscriptionListTest.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * Copyright 2018 Sage Intacct, Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not
+ * use this file except in compliance with the License. You may obtain a copy 
+ * of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * or in the "LICENSE" file accompanying this file. This file is distributed on 
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
+ * express or implied. See the License for the specific language governing 
+ * permissions and limitations under the License.
+ */
+
+using Intacct.SDK.Functions.Company;
+using Intacct.SDK.Tests.Xml;
+using Xunit;
+
+namespace Intacct.SDK.Tests.Functions.Company
+{
+    public class SubscriptionListTest : XmlObjectTestHelper
+    {
+        [Fact]
+        public void GetXmlTest()
+        {
+            string expected = @"<?xml version=""1.0"" encoding=""utf-8""?>
+<function controlid=""unittest"">
+    <get_applications />
+</function>";
+
+            SubscriptionList apiFunction = new SubscriptionList("unittest");
+
+            this.CompareXml(expected, apiFunction);
+        }
+    }
+}
---
+++ Intacct.SDK/Functions/Company/SubscriptionList.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Copyright 2018 Sage Intacct, Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not
+ * use this file except in compliance with the License. You may obtain a copy 
+ * of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * or in the "LICENSE" file accompanying this file. This file is distributed on 
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
+ * express or implied. See the License for the specific language governing 
+ * permissions and limitations under the License.
+ */
+
+using Intacct.SDK.Xml;
+
+namespace Intacct.SDK.Functions.Company
+{
+    public class SubscriptionList : AbstractFunction
+    {
+        public SubscriptionList(string controlId = "") : base(controlId)
+        {
+        }
+
+        public override void WriteXml(ref IaXmlWriter xml)
+        {
+            xml.WriteStartElement("function");
+            xml.WriteAttributeString("controlid", ControlId);
+
+            xml.WriteElementString("get_applications", "");
+
+            xml.WriteEndElement(); //function
+        }
+    }
+}
