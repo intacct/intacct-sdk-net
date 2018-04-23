052bca445447b5e8e8b4003dc0b2270f14aed1a5
James <jimmymcpeter@users.noreply.github.com>
2018-04-23 09:44:01 -0700

    Resolve issue #30 (#39)
    Adding functions to
     - list subscriptions, and
    - get user effective permissions

---
+++ Intacct.SDK.Tests/Functions/Company/UserEffectivePermissionListTest.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Copyright 2018 Sage Intacct, Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not
+ * use this file except in compliance with the License. You may obtain a copy 
+ * of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * or in the "LICENSE" file accompanying this file. This file is distributed on 
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
+ * express or implied. See the License for the specific language governing 
+ * permissions and limitations under the License.
+ */
+
+using Intacct.SDK.Functions.Company;
+using Intacct.SDK.Tests.Xml;
+using Xunit;
+
+namespace Intacct.SDK.Tests.Functions.Company
+{
+    public class UserEffectivePermissionListTest : XmlObjectTestHelper
+    {
+        [Fact]
+        public void GetXmlTest()
+        {
+            string expected = @"<?xml version=""1.0"" encoding=""utf-8""?>
+<function controlid=""unittest"">
+    <getUserPermissions>
+        <userId>USERID</userId>
+    </getUserPermissions>
+</function>";
+
+            UserEffectivePermissionList apiFunction = new UserEffectivePermissionList("unittest")
+            {
+                UserId = "USERID"
+            };
+
+            this.CompareXml(expected, apiFunction);
+        }
+    }
+}
