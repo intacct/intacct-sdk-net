46ff2ca72c0a66f8fea02d2f90f064de5a1c631f
Coleen Ho <cho@intacct.com>
2018-04-27 09:20:46 -0700

    Adding PurchasingTransactionUpdate, PurchasingTransactionLineUpdate, and unit tests.

---
+++ Intacct.SDK.Tests/Functions/Purchasing/PurchasingTransactionUpdateTest.cs
@@ -0,0 +1,169 @@
+﻿/*
+ * Copyright 2018 Sage Intacct, Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not
+ * use this file except in compliance with the License. You may obtain a copy 
+ * of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * or in the "LICENSE" file accompanying this file. This file is distributed on 
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
+ * express or implied. See the License for the specific language governing 
+ * permissions and limitations under the License.
+ */
+
+using System;
+using System.Collections.Generic;
+using Intacct.SDK.Functions.InventoryControl;
+using Intacct.SDK.Functions.Purchasing;
+using Intacct.SDK.Tests.Xml;
+using Xunit;
+
+namespace Intacct.SDK.Tests.Functions.Purchasing
+{
+    public class PurchasingTransactionUpdateTest : XmlObjectTestHelper
+    {
+        [Fact]
+        public void GetXmlTest()
+        {
+            string expected = @"<?xml version=""1.0"" encoding=""utf-8""?>
+<function controlid=""unittest"">
+    <update_potransaction key=""Purchase Order-PO0213"">
+        <updatepotransitems />
+    </update_potransaction>
+</function>";
+
+            PurchasingTransactionUpdate record = new PurchasingTransactionUpdate("unittest")
+            {
+                DocumentId = "Purchase Order-PO0213",
+            };
+
+            this.CompareXml(expected, record);
+        }
+        
+        [Fact]
+        public void GetAllXmlTest()
+        {
+            string expected = @"<?xml version=""1.0"" encoding=""utf-8""?>
+<function controlid=""unittest"">
+    <update_potransaction key=""PO-2234"">
+        <transactiontype>Purchase Order</transactiontype>
+        <datecreated>
+            <year>2015</year>
+            <month>06</month>
+            <day>30</day>
+        </datecreated>
+        <dateposted>
+            <year>2015</year>
+            <month>06</month>
+            <day>30</day>
+        </dateposted>
+        <createdfrom>Purchase Order-P1002</createdfrom>
+        <vendorid>23530</vendorid>
+        <documentno>23430</documentno>
+        <referenceno>234235</referenceno>
+        <termname>N30</termname>
+        <datedue>
+            <year>2020</year>
+            <month>09</month>
+            <day>24</day>
+        </datedue>
+        <message>Submit</message>
+        <shippingmethod>USPS</shippingmethod>
+        <returnto>
+            <contactname>Bobbi Reese</contactname>
+        </returnto>
+        <payto>
+            <contactname>Henry Jones</contactname>
+        </payto>
+        <supdocid>6942</supdocid>
+        <externalid>20394</externalid>
+        <basecurr>USD</basecurr>
+        <currency>USD</currency>
+        <exchratedate>
+            <year>2015</year>
+            <month>06</month>
+            <day>30</day>
+        </exchratedate>
+        <exchratetype>Intacct Daily Rate</exchratetype>
+        <customfields>
+            <customfield>
+                <customfieldname>customfield1</customfieldname>
+                <customfieldvalue>customvalue1</customfieldvalue>
+            </customfield>
+        </customfields>
+        <state>Pending</state>
+        <updatepotransitems>
+            <updatepotransitem line_num=""1"">
+                <itemid>02354032</itemid>
+            </updatepotransitem>
+            <potransitem>
+                <itemid>2390552</itemid>
+                <quantity>223</quantity>
+            </potransitem>
+        </updatepotransitems>
+        <updatesubtotals>
+            <subtotal>
+                <description>Subtotal description</description>
+                <total>223</total>
+            </subtotal>
+        </updatesubtotals>
+    </update_potransaction>
+</function>";
+
+            PurchasingTransactionUpdate record = new PurchasingTransactionUpdate("unittest")
+            {
+                DocumentId = "PO-2234",
+                TransactionDefinition = "Purchase Order",
+                TransactionDate = new DateTime(2015, 06, 30),
+                GlPostingDate = new DateTime(2015, 06, 30),
+                CreatedFrom = "Purchase Order-P1002",
+                VendorId = "23530",
+                DocumentNumber = "23430",
+                ReferenceNumber = "234235",
+                PaymentTerm = "N30",
+                DueDate = new DateTime(2020, 09, 24),
+                Message = "Submit",
+                ShippingMethod = "USPS",
+                ReturnToContactName = "Bobbi Reese",
+                PayToContactName = "Henry Jones",
+                AttachmentsId = "6942",
+                ExternalId = "20394",
+                BaseCurrency = "USD",
+                TransactionCurrency = "USD",
+                ExchangeRateDate = new DateTime(2015, 06, 30),
+                ExchangeRateType = "Intacct Daily Rate",
+                State = "Pending",
+                CustomFields = new Dictionary<string, dynamic>
+                {
+                    { "customfield1", "customvalue1" }
+                }
+            };
+
+            PurchasingTransactionLineUpdate line1 = new PurchasingTransactionLineUpdate()
+            {
+                LineNo = 1,
+                ItemId = "02354032",
+            };
+
+            record.Lines.Add(line1);
+
+            PurchasingTransactionLineCreate line2 = new PurchasingTransactionLineCreate()
+            {
+                ItemId = "2390552",
+                Quantity = 223,
+            };
+            record.Lines.Add(line2);
+
+            TransactionSubtotalCreate subtotal1 = new TransactionSubtotalCreate()
+            {
+                Description = "Subtotal description",
+                Total = 223,
+            };
+            record.Subtotals.Add(subtotal1);
+            
+            this.CompareXml(expected, record);
+        }
+    }
+}
---
+++ Intacct.SDK/Functions/Purchasing/PurchasingTransactionUpdate.cs
@@ -0,0 +1,140 @@
+﻿/*
+ * Copyright 2018 Sage Intacct, Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not
+ * use this file except in compliance with the License. You may obtain a copy 
+ * of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * or in the "LICENSE" file accompanying this file. This file is distributed on 
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
+ * express or implied. See the License for the specific language governing 
+ * permissions and limitations under the License.
+ */
+
+using Intacct.SDK.Functions.InventoryControl;
+using Intacct.SDK.Xml;
+
+namespace Intacct.SDK.Functions.Purchasing
+{
+    public class PurchasingTransactionUpdate : AbstractPurchasingTransaction
+    {
+
+        public PurchasingTransactionUpdate(string controlId = null) : base(controlId)
+        {
+        }
+
+        public override void WriteXml(ref IaXmlWriter xml)
+        {
+            xml.WriteStartElement("function");
+            xml.WriteAttribute("controlid", ControlId, true);
+
+            xml.WriteStartElement("update_potransaction");
+            xml.WriteAttribute("key", DocumentId, true);
+
+            xml.WriteElement("transactiontype", TransactionDefinition);
+
+            if (TransactionDate.HasValue)
+            {
+                xml.WriteStartElement("datecreated");
+                xml.WriteDateSplitElements(TransactionDate.Value);
+                xml.WriteEndElement(); //datecreated   
+            }
+
+            if (GlPostingDate.HasValue)
+            {
+                xml.WriteStartElement("dateposted");
+                xml.WriteDateSplitElements(GlPostingDate.Value);
+                xml.WriteEndElement(); //dateposted
+            }
+
+            xml.WriteElement("createdfrom", CreatedFrom);
+            xml.WriteElement("vendorid", VendorId);
+            xml.WriteElement("documentno", DocumentNumber);
+
+            xml.WriteElement("referenceno", ReferenceNumber);
+            xml.WriteElement("vendordocno", VendorDocNumber);
+            xml.WriteElement("termname", PaymentTerm);
+
+            if (DueDate.HasValue)
+            {
+                xml.WriteStartElement("datedue");
+                xml.WriteDateSplitElements(DueDate.Value);
+                xml.WriteEndElement(); //datedue   
+            }
+            
+            xml.WriteElement("message", Message);
+            xml.WriteElement("shippingmethod", ShippingMethod);
+
+            if (!string.IsNullOrWhiteSpace(ReturnToContactName))
+            {
+                xml.WriteStartElement("returnto");
+                xml.WriteElement("contactname", ReturnToContactName);
+                xml.WriteEndElement(); //returnto                
+            }
+
+            if (!string.IsNullOrWhiteSpace(PayToContactName))
+            {
+                xml.WriteStartElement("payto");
+                xml.WriteElement("contactname", PayToContactName);
+                xml.WriteEndElement(); //payto                
+            }
+
+            xml.WriteElement("supdocid", AttachmentsId);
+            xml.WriteElement("externalid", ExternalId);
+            
+            xml.WriteElement("basecurr", BaseCurrency);
+            xml.WriteElement("currency", TransactionCurrency);
+
+            if (ExchangeRateDate.HasValue)
+            {
+                xml.WriteStartElement("exchratedate");
+                xml.WriteDateSplitElements(ExchangeRateDate.Value);
+                xml.WriteEndElement(); //exchratedate
+            }
+            if (!string.IsNullOrWhiteSpace(ExchangeRateType))
+            {
+                xml.WriteElement("exchratetype", ExchangeRateType);
+            }
+            else if (ExchangeRateValue.HasValue)
+            {
+                xml.WriteElement("exchrate", ExchangeRateValue);
+            }
+            else if (!string.IsNullOrWhiteSpace(BaseCurrency) || !string.IsNullOrWhiteSpace(TransactionCurrency))
+            {
+                xml.WriteElement("exchratetype", ExchangeRateType, true);
+            }
+            
+            xml.WriteCustomFieldsExplicit(CustomFields);
+
+            xml.WriteElement("state", State);
+
+            xml.WriteStartElement("updatepotransitems");
+            if (Lines.Count > 0)
+            {
+                foreach (AbstractPurchasingTransactionLine line in Lines)
+                {
+                    line.WriteXml(ref xml);
+                }
+            }
+            xml.WriteEndElement(); //updatepotransitems
+
+            if (Subtotals.Count > 0)
+            {
+                xml.WriteStartElement("updatesubtotals");
+                foreach (AbstractTransactionSubtotal subtotal in Subtotals)
+                {
+                    subtotal.WriteXml(ref xml);
+                }
+                xml.WriteEndElement(); //updatesubtotals
+            }
+
+            xml.WriteEndElement(); //update_potransaction
+
+            xml.WriteEndElement(); //function
+        }
+
+        
+    }
+}
