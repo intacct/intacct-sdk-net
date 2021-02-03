574f8540f820c12b0c5aed9ab042d54bdf506a69
Motty Kohn <mkohn@snfmetrics.com>
2021-02-03 13:24:24 -0500

    add lookup to NewFunction

---
+++ Intacct.SDK/Functions/Common/NewQuery/Lookup.cs
@@ -0,0 +1,23 @@
+namespace Intacct.SDK.Functions.Common.NewQuery
+{
+    public class Lookup : AbstractFunction
+    {
+        public string Object;
+
+        public Lookup(string controlId = "") : base(controlId)
+        {
+        }
+
+        public override void WriteXml(ref IaXmlWriter xml)
+        {
+            xml.WriteStartElement("function");
+            xml.WriteAttributeString("controlid", ControlId);
+
+            xml.WriteStartElement("lookup");
+            xml.WriteElementString("object", Object);
+            xml.WriteEndElement(); //lookup
+
+            xml.WriteEndElement(); //function
+        }
+    }
+}
