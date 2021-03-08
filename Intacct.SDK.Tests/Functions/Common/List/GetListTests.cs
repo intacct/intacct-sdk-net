644ef6a30dbe8e904ecf738fb0ef46895f36eba3
Craig Buchanan <craig.buchanan@cogniza.com>
2021-03-08 11:21:28 -0600

    initial commit

---
+++ Intacct.SDK.Tests/Functions/Common/List/GetListTests.cs
@@ -0,0 +1,142 @@
+﻿/*
+ * Copyright 2020 Sage Intacct, Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not
+ * use this file except in compliance with the License. You may obtain a copy 
+ * of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * or in the "LICENSE" file accompanying this file. This file is distributed on 
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
+ * express or implied. See the License for the specific language governing 
+ * permissions and limitations under the License.
+ */
+
+using Xunit;
+using Intacct.SDK.Functions.Common.List;
+using Intacct.SDK.Tests.Xml;
+using System.Collections.Generic;
+
+namespace Intacct.SDK.Tests.Functions.Common.List
+{
+    public class GetListTests : XmlObjectTestHelper
+    {
+		[Fact]
+		public void MinimalConstructor()
+		{
+            // arrange
+            string expected = @"<?xml version=""1.0"" encoding=""utf-8""?>
+<function>
+    <get_list object=""supdoc"" start=""0"" maxitems=""100"" showprivate=""true"">
+        <filter />
+        <fields />
+        <sorts />
+    </get_list>
+</function>";
+
+            // act
+            GetList getList = new GetList("supdoc");
+
+            // assert
+            this.CompareXml(expected, getList);
+        }
+
+        [Fact]
+        public void ExpressionFilterProvided()
+        {
+            // arrange
+            string expected = @"<?xml version=""1.0"" encoding=""utf-8""?>
+<function>
+    <get_list object=""supdoc"" start=""0"" maxitems=""100"" showprivate=""true"">
+        <filter>
+            <expression>
+                <field>FieldA</field>
+                <operator>=</operator>
+                <value>Lorem Ipsum</value>
+            </expression>
+        </filter>
+        <fields />
+        <sorts />
+    </get_list>
+</function>";
+
+            ExpressionFilter expression = new ExpressionFilter()
+            { 
+                Field = "FieldA",
+                Operator = "=",
+                Value = "Lorem Ipsum"
+            };
+
+            // act
+            GetList getList = new GetList("supdoc")
+            {
+                Expression = expression
+            };
+
+            // assert
+            this.CompareXml(expected, getList);
+        }
+
+        [Fact]
+        public void FieldsProvided()
+        {
+            // arrange
+            string expected = @"<?xml version=""1.0"" encoding=""utf-8""?>
+<function>
+    <get_list object=""supdoc"" start=""0"" maxitems=""100"" showprivate=""true"">
+        <filter />
+        <fields>
+            <field>FieldA</field>
+            <field>FieldB</field>
+        </fields>
+        <sorts />
+    </get_list>
+</function>";
+
+            List<string> fields = new List<string>();
+            fields.Add("FieldA");
+            fields.Add("FieldB");
+
+            // act
+            GetList getList = new GetList("supdoc")
+            {
+                Fields = fields
+            };
+
+            // assert
+            this.CompareXml(expected, getList);
+        }
+
+        [Fact]
+        public void SortsProvided()
+        {
+            // arrange
+            string expected = @"<?xml version=""1.0"" encoding=""utf-8""?>
+<function>
+    <get_list object=""supdoc"" start=""0"" maxitems=""100"" showprivate=""true"">
+        <filter />
+        <fields />
+        <sorts>
+            <sortfield order=""asc"">FieldA</sortfield>
+            <sortfield order=""desc"">FieldB</sortfield>
+        </sorts>
+    </get_list>
+</function>";
+
+            List<SortedField> sortedFields = new List<SortedField>();
+            sortedFields.Add(new SortedField("FieldA"));
+            sortedFields.Add(new SortedField("FieldB","desc"));
+
+            // act
+            GetList getList = new GetList("supdoc")
+            {
+                SortedFields = sortedFields
+            };
+
+            // assert
+            this.CompareXml(expected, getList);
+        }
+
+    }
+}
---
+++ Intacct.SDK/Functions/Common/List/GetList.cs
@@ -0,0 +1,100 @@
+﻿/*
+ * Copyright 2020 Sage Intacct, Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not
+ * use this file except in compliance with the License. You may obtain a copy 
+ * of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * or in the "LICENSE" file accompanying this file. This file is distributed on 
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
+ * express or implied. See the License for the specific language governing 
+ * permissions and limitations under the License.
+ */
+
+using Intacct.SDK.Xml;
+
+namespace Intacct.SDK.Functions.Common.List
+{
+    public class GetList : AbstractFunction
+    {
+        public int start;
+        public int maxitems;
+        public bool showprivate;
+        public string ObjectName;
+
+        public System.Collections.Generic.List<string> Fields = new System.Collections.Generic.List<string>();
+        public System.Collections.Generic.List<SortedField> SortedFields = new System.Collections.Generic.List<SortedField>();
+
+        public ExpressionFilter Expression;
+
+        public GetList(string objectName)
+        {
+            this.start = 0;
+            this.maxitems = 100;
+            this.showprivate = true;
+            this.ObjectName = objectName;
+        }
+
+        public override void WriteXml(ref IaXmlWriter xml)
+        {
+            xml.WriteStartDocument();
+
+            xml.WriteStartElement("function");
+            //xml.WriteAttribute("controlid", ControlId, true);
+
+            xml.WriteStartElement("get_list");
+            xml.WriteAttributeString("object", this.ObjectName);
+            xml.WriteAttributeString("start", this.start.ToString());
+            xml.WriteAttributeString("maxitems", this.maxitems.ToString());
+            xml.WriteAttributeString("showprivate", this.showprivate.ToString().ToLower());
+
+            xml.WriteStartElement("filter");
+
+            if (this.Expression != null)
+            {
+                xml.WriteStartElement("expression");
+                xml.WriteElementString("field", this.Expression.Field);
+                xml.WriteElementString("operator", this.Expression.Operator);
+                xml.WriteElementString("value", this.Expression.Value);
+                xml.WriteEndElement(); // </expression>
+            }
+
+            // if ( this.logical ) 
+            // {
+            //     xml.WriteStartElement("logical");
+            //     xml.WriteEndElement(); // </logical>
+            // }
+
+            xml.WriteEndElement(); // </filter>
+
+            xml.WriteStartElement("fields");
+            foreach (string Field in this.Fields)
+            {
+                xml.WriteElementString("field", Field);
+            }
+            xml.WriteEndElement(); // </fields>
+
+            xml.WriteStartElement("sorts");
+            foreach (SortedField SortedField in this.SortedFields)
+            {
+                xml.WriteStartElement("sortfield");
+                xml.WriteAttributeString("order", SortedField.Order);
+                xml.WriteString(SortedField.Name);
+                xml.WriteEndElement(); // </sortfield>
+            }
+            xml.WriteEndElement(); // </sorts>
+
+            xml.WriteEndElement(); // </get_list> 
+
+            xml.WriteEndElement(); // </function> 
+
+            xml.WriteEndDocument();
+            xml.Flush();
+            xml.Close();
+
+        }
+    }
+
+}
